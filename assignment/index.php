6a26f61576a62282a548655a2664cf9ca1a1fd4f
ollylll97lll <58421531+ollylll97lll@users.noreply.github.com>
2019-12-26 04:29:24 +0700

    add php redirect modified index components add alert .js function

--- assignment/index.php
+++ assignment/index.php
@@ -14,22 +14,22 @@ if ($conn->connect_error) {
 
 $fName = $_POST['fName'];
 $lName = $_POST['lName'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
+$Email = $_POST['Email'];
+$Phone = $_POST['Phone'];
 
-  if (isset($_POST['createweb'])) $createweb = true;  else $createweb = false;
-  if (isset($_POST['hire'])) $hire = true;  else $hirre = false;
-  if (isset($_POST['moreinfo'])) $moreinfo = true;  else $moreinfo = false;
+  if (isset($_POST['nweb'])) $nweb = true;  else $nweb = false;
+  if (isset($_POST['hemp'])) $hemp = true;  else $hemp = false;
+  if (isset($_POST['info'])) $info = true;  else $info = false;
 
 $reference = $_POST['reference'];
-$questions = $_POST['questions'];
+$question = $_POST['question'];
 
-$query = "INSERT into consult(fname, lname, email, phone, createweb, hire, moreinfo, reference, questions)
-VALUES('$fName', '$lName', '$email', '$phone', '$createweb', '$hire', '$moreinfo', '$reference', '$questions')";
+$query = "INSERT INTO consult(fname, lname, email, phone, createweb, hire, moreinfo, reference, questions)
+VALUES('$fName', '$lName', '$Email', '$Phone', '$nweb', '$hemp', '$info', '$reference', '$question')";
 
 $results = $conn->query($query);
 
-echo "string";
+//echo "string";
 if(!$results){
   echo " insert failed"."<br/>";
 }
@@ -48,4 +48,5 @@ while ($row = mysqli_fetch_array($results)) {
   echo
   $row['id']." ".$row['fname']." ".$row['lname']." ".$row['email']." ".$row['phone']."<br/>";
 }
+$conn --> close();
 ?>
---
+++ assignment/scripts/myFunctions.js
@@ -0,0 +1,4 @@
+//thank you alert
+function tyAlert(){
+  alert("thank you for your interest in company. we will contact you soon");
+}
